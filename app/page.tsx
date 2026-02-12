473e88aba7ac2170d3ad780b99b2640262dd710c
nuroscript <nuroscript@gmail.com>
2026-02-12 17:48:39 +0530

    36 commit

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,6 @@ export default function Home() {
 
         </div>
 
-
         <div className="h-full w-[90%] lg:w-[55%] flex flex-col items-center justify-center gap-5">
 
           <h1 className=" text-xl lg:text-2xl text-white">Powerful Web Applications</h1>
@@ -93,16 +92,16 @@ export default function Home() {
 
       </div>
 
-      <div className="w-full h-[550px] flex items-center p-5 gap-5">
+      <div className="w-full h-[590px] lg:h-[550px] pb-5 lg:pb-0 flex flex-col lg:flex-row items-center p-5 gap-5">
 
-        <div className="h-full w-[55%] flex flex-col items-center justify-center gap-5">
+        <div className="h-[40%] w-full lg:w-[55%] flex flex-col items-center justify-center gap-5">
 
-          <h1 className="text-2xl text-white">High-Performance Mobile Apps</h1>
+          <h1 className=" text-xl lg:text-2xl text-white">High-Performance Mobile Apps</h1>
           <p className="text-white/80 text-sm text-center w-[80%]">We develop reliable, scalable, and user-friendly mobile applications tailored for Android and iOS. From intuitive UI to powerful backend integration, our apps are built for speed, security, and growth.</p>
 
         </div>
 
-        <div className="h-[80%] w-[45%] relative">
+        <div className="h-[60%] lg:h-[85%] w-full lg:w-[45%] relative mb-5 lg:mb-0">
 
           <Image
             src="/pho.png"
@@ -116,9 +115,9 @@ export default function Home() {
 
       </div>
 
-      <div className="w-full h-[550px] flex items-center p-5 gap-5 bg-[#131313]">
+      <div className="w-full h-[550px] flex flex-col lg:flex-row items-center p-5 lg:gap-5 bg-[#131313]">
 
-        <div className="h-[90%] w-[45%] relative">
+        <div className="h-[90%] w-[95%] lg:w-[45%] relative my-5 lg:my-0">
 
           <Image
             src="/Gaming.png"
@@ -131,9 +130,9 @@ export default function Home() {
         </div>
 
 
-        <div className="h-full w-[55%] flex flex-col items-center justify-center gap-5">
+        <div className="h-full w-[90%] lg:w-[55%] flex flex-col items-center justify-center gap-5">
 
-          <h1 className="text-2xl text-white">Next-Gen Tablet Games</h1>
+          <h1 className=" text-xl lg:text-2xl text-white">Next-Gen Tablet Games</h1>
           <p className="text-white/80 text-sm text-center w-[80%]">From casual to interactive gameplay, we build tablet games that are visually stunning, responsive, and optimized for performance across all tablet devices.</p>
 
         </div>
@@ -141,16 +140,16 @@ export default function Home() {
       </div>
 
 
-      <div className="w-full h-[550px] flex items-center p-5 gap-5">
+      <div className="w-full h-[550px] pb-5 lg:pb-0 flex flex-col lg:flex-row items-center p-5 gap-5">
 
-        <div className="h-full w-[55%] flex flex-col items-center justify-center gap-5">
+        <div className="h-full w-[90%] lg:w-[55%] flex flex-col items-center justify-center gap-5">
 
-          <h1 className="text-2xl text-white">Engaging Mobile Game Development</h1>
+          <h1 className=" text-xl lg:text-2xl text-white">Engaging Mobile Game Development</h1>
           <p className="text-white/80 text-sm text-center w-[80%]">We create high-quality mobile games with smooth gameplay, eye-catching visuals, and optimized performance. Built for Android and iOS to keep players engaged and coming back.</p>
 
         </div>
 
-        <div className="h-[80%] w-[40%] relative">
+        <div className="lg:h-[80%] lg:w-[40%] h-[90%] w-[90%] relative">
 
           <Image
             src="/gamingM.png"
@@ -164,10 +163,9 @@ export default function Home() {
 
       </div>
 
+      <div className="w-full h-[550px] flex flex-col lg:flex-row items-center p-5 lg:gap-5 bg-[#131313]">
 
-      <div className="w-full h-[550px] flex items-center p-5 gap-5 bg-[#131313]">
-
-        <div className="h-[75%] w-[50%] relative">
+        <div className="lg:h-[75%] lg:w-[50%] h-[90%] w-[90%] relative">
 
           <Image
             src="/TV.png"
@@ -179,9 +177,9 @@ export default function Home() {
 
         </div>
 
-        <div className="h-full w-[55%] flex flex-col items-center justify-center gap-5">
+        <div className="h-full w-[90%] lg:w-[55%] flex flex-col items-center justify-center gap-5">
 
-          <h1 className="text-2xl text-white">Smart TV App Development</h1>
+          <h1 className="text-xl lg:text-2xl text-white">Smart TV App Development</h1>
           <p className="text-white/80 text-sm text-center w-[80%]">We build fast, intuitive, and visually rich TV applications optimized for large screens, remote navigation, and smooth performance across Smart TV platforms.</p>
 
         </div>
@@ -190,16 +188,16 @@ export default function Home() {
 
 
       
-      <div className="w-full h-[550px] flex items-center p-5 gap-5">
+      <div className="w-full h-[560px] lg:h-[550px] pb-5 lg:pb-0 flex flex-col lg:flex-row items-center p-5 gap-5">
 
-        <div className="h-full w-[55%] flex flex-col items-center justify-center gap-5">
+        <div className="lg:h-full h-[30%] w-[90%] lg:w-[55%] flex flex-col items-center justify-center gap-5 mt-10 lg:mt-0">
 
-          <h1 className="text-2xl text-white">Apps for Wearables</h1>
+          <h1 className=" text-xl lg:text-2xl text-white">Apps for Wearables</h1>
           <p className="text-white/80 text-sm text-center w-[80%]">We build lightweight, fast, and intuitive smartwatch applications designed for quick interactions, real-time data, and seamless performance on wearable devices.</p>
 
         </div>
 
-        <div className="h-[90%] w-[45%] relative">
+        <div className="lg:h-[90%] lg:w-[45%] h-[70%] w-full relative">
 
           <Image
             src="/watch.png"
